622c2b8b073221caf66045e75797f055f4853c5d
Clark Smith <clark.smith79@gmail.com>
2021-05-25 19:21:48 -0600

    mobile tweaks

---
+++ frontend/build/precache-manifest.75c31bcd60c745eb7ad4a4b4efd9bc3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3b488b61c255c5ca9ff70de650dfa3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716fe7e0e9eef9c470df",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "71c0eaa38a7888c8d1b2",
+    "url": "/static/css/main.3d1b291e.chunk.css"
+  },
+  {
+    "revision": "716fe7e0e9eef9c470df",
+    "url": "/static/js/2.04142218.chunk.js"
+  },
+  {
+    "revision": "ded7d7badaa34847250951fc6b03b282",
+    "url": "/static/js/2.04142218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c0eaa38a7888c8d1b2",
+    "url": "/static/js/main.5abbef55.chunk.js"
+  },
+  {
+    "revision": "e3d2e70438929ee03f9b",
+    "url": "/static/js/runtime-main.760e8108.js"
+  }
+]);
